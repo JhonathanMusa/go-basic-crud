c63476c1c4acd81a98c66caec044baf451b11fed
jhonathanmusa <jhonathansamu91@gmail.com>
2021-01-29 11:44:28 -0500

    adding get by id method

--- main.go
+++ main.go
@@ -21,6 +21,18 @@ func GetUsers(w http.ResponseWriter, req *http.Request) {
 	json.NewEncoder(w).Encode(users)
 }
 
+func GetUserById(w http.ResponseWriter, req *http.Request) {
+	params := mux.Vars(req)
+
+	for _, item := range users {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Users{})
+}
+
 func CreateNewUser(w http.ResponseWriter, req *http.Request) {
 	params := mux.Vars(req)
 	var user Users
@@ -38,6 +50,7 @@ func main() {
 	users = append(users, Users{ID: "1", FirstName: "jhonathan", LastName: "salazar"})
 
 	router.HandleFunc("/users", GetUsers).Methods("GET")
+	router.HandleFunc("/user/{id}", GetUserById).Methods("GET")
 	router.HandleFunc("/new-user/{id}", CreateNewUser).Methods("POST")
 
 	log.Fatal(http.ListenAndServe(":3000", router))
